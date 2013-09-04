788af98c0ae6ef888f35cb40820eaaa2a354f110
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2013-09-04 16:56:11 +0200

    Update dtm function

--- routes/index.js
+++ routes/index.js
@@ -16,14 +16,19 @@ exports.dtm = function(req, res){
 
   var box = req.query.box.split(','); // The bounding box from query
   var exec = require('child_process').exec;
-
+  var fs = require('fs');
+  var filename = "/tmp/"+box.join("_")+".png";
   function pipe(err,stdout, stderr) {
     if (err) {
       err.error = stderr
       res.send(err);
+      return;
     }
-    res.send(stdout);
+    res.writeHead(200, {'Content-Type': 'image/png' });
+    var img = fs.readFileSync(filename);
+    res.end(img, 'binary');
+    fs.unlinkSync(filename);
   }
-
-  exec("gdal_translate -scale 0 550 -of PNG -outsize 2000 2000 -projwin "+box.join(', ')+" /mnt/warez/dtm/dtm.vrt >(tee /tmp/dtm.png)", pipe);
+  var command = "bash -c 'gdal_translate -q -scale 0 550 -ot Byte -of PNG -outsize 2000 2000 -projwin " + box.join(', ') + " /mnt/warez/dtm/dtm.vrt "+filename+"'"
+  exec(command, pipe);
 };
