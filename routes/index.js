5d4c9753952de990695ff2e678fcb985b42de516
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2013-09-04 15:29:34 +0200

    First test on dtm data

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,24 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Hello Terra!', version: require("../package.json").version });
 };
+
+
+/*
+ * GET DTM data.
+ */
+
+exports.dtm = function(req, res){
+
+  var box = req.query.box.split(','); // The bounding box from query
+  var exec = require('child_process').exec;
+
+  function pipe(err,stdout, stderr) {
+    if (err) {
+      err.error = stderr
+      res.send(err);
+    }
+    res.send(stdout);
+  }
+
+  exec("gdal_translate -scale 0 550 -of PNG -outsize 2000 2000 -projwin "+box.join(', ')+" /mnt/warez/dtm/dtm.vrt >(tee /tmp/dtm.png)", pipe);
+};
--- server.js
+++ server.js
@@ -37,6 +37,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/dtm', routes.dtm);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
