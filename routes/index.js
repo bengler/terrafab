7e46a01640e5cbd4e64b412c98f96afdaeb57273
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2013-09-05 10:10:13 +0200

    Add some error messages

--- routes/index.js
+++ routes/index.js
@@ -15,13 +15,21 @@ exports.index = function(req, res){
 exports.dtm = function(req, res){
 
   var box = req.query.box.split(','); // The bounding box from query
+  if(box == null) {
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    return res.send('{"error": "No box param given. Should be in format: ?box=253723.176600653,6660500.4670516,267723.176600653,6646500.4670516"}');
+  }
   var outsize = req.query.outsize; // The output size of the tile
   if(outsize == null) {
-    outsize = 2000;
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    return res.send('{"error": "No outsize param given. Should be in format: ?outsize=1000,1000  (x,y)"}');
+  } else {
+    outsize = outsize.split(",");
   }
+
   var png_file = "/tmp/"+box.join("_")+".png";
   var dtm_file = "/mnt/warez/dtm/dtm.vrt";
-  var command = "bash -c 'gdal_translate -q -scale 0 550 -ot Byte -of PNG -outsize "+outsize+" "+outsize+" -projwin " + box.join(', ') + " "+dtm_file+" "+png_file+"'";
+  var command = "bash -c 'gdal_translate -q -scale 0 550 -ot Byte -of PNG -outsize "+outsize[0]+" "+outsize[1]+" -projwin " + box.join(', ') + " "+dtm_file+" "+png_file+"'";
 
   var exec = require('child_process').exec;
   var fs = require('fs');
