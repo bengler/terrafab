46d25ef4d8617edcae64bdec559c758893b080bc
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2013-09-17 14:52:45 +0200

    Working X3D exporter, yay

--- fabricator/to_x3d.js
+++ fabricator/to_x3d.js
@@ -1,16 +1,12 @@
 var THREE = require("three");
 
-// function stringifyVector(vec){
-//   return ""+vec.x+" "+vec.z+" "+vec.y;
-// }
-
-// function stringifyVertex(vec){
-//   return "vertex "+stringifyVector(vec)+" \n";
-// }
+function coordFromVector(vec){
+  return ""+vec.x+" "+vec.z+" "+vec.y;
+}
 
 function toX3D(geometry, name){
   var vertices = geometry.vertices;
-  var tris     = geometry.faces;
+  var faces    = geometry.faces;
 
   geometry = THREE.GeometryUtils.triangulateQuads( geometry );
 
@@ -21,7 +17,7 @@ function toX3D(geometry, name){
   x3d.header = "\n\
     <head>\n\
       <meta content='"+name+".x3d' name='title'/>\n\
-      <meta content='Export from Terrafab' name='description'/>\n\
+      <meta content='X3D export from Bengler Terrafab' name='description'/>\n\
       <meta content='"+(new Date()).toString()+"' name='created'/>\n\
       <meta content='"+(new Date()).toString()+"' name='modified'/>\n\
       <meta content='Bengler TerraFab' name='creator'/>\n\
@@ -33,31 +29,28 @@ function toX3D(geometry, name){
     </head>\n";
 
   x3d.scene = "\n\
-    <Scene>\n\
-      <Group>\n\
-        <Viewpoint centerOfRotation='0 -1 0' description='Hello world!' position='0 -1 7'/>\n\
-        <Transform rotation='0 1 0 3'>\n\
-          <Shape>\n\
-            <Sphere/>\n\
-            <Appearance>\n\
-              <Material diffuseColor='0 0.5 1'/>\n\
-              <ImageTexture url='\"texture.png\"'/>\n\
-            </Appearance>\n\
-          </Shape>\n\
-        </Transform>\n\
-    </Scene>\n\n"
-  // for(var i = 0; i<tris.length; i++){
-  //   stl += ("facet normal "+stringifyVector( tris[i].normal )+"\n");
-  //   stl += ("outer loop\n");
-  //   stl += stringifyVertex( vertices[ tris[i].a ]);
-  //   stl += stringifyVertex( vertices[ tris[i].c ]);
-  //   stl += stringifyVertex( vertices[ tris[i].b ]);
-  //   stl += ("endloop\n");
-  //   stl += ("endfacet\n");
-  // }
-  // stl += ("endsolid model\n");
-
+    <Scene>\n"
+        x3d.scene += "         <Shape>\n\
+            <IndexedTriangleSet solid='false' index='";
+        indicies = [];
+        for(var i = 0; i<faces.length; i++){
+          indicies.push([faces[i].a, faces[i].c, faces[i].b].join(' '));
+        }
+        x3d.scene += indicies.join(' ');
+        x3d.scene += "'>\n";
+        x3d.scene += "              <Coordinate point='";
+        points = [];
+        for(var i = 0; i<vertices.length; i++){
+          points.push(coordFromVector(vertices[i]));
+        }
+        x3d.scene += points.join(', ')
+        x3d.scene += "'/>\n";
+        x3d.scene += "            </IndexedTriangleSet>\n";
+        x3d.scene += "         </Shape>\n";
+  x3d.scene += "\
+    </Scene>\n\n";
   return x3d.doc+x3d.header+x3d.scene+'</X3D>\n';
 }
+
 
 module.exports = toX3D;
