ad7565d75adc3914618852d96d26beba16473373
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2013-09-17 18:31:25 +0200

    Add image texture Appearance element + clean code

--- fabricator/to_x3d.js
+++ fabricator/to_x3d.js
@@ -10,14 +10,15 @@ function toX3D(geometry, name){
 
   geometry = THREE.GeometryUtils.triangulateQuads( geometry );
 
-  x3d = {};
-  x3d.doc = "<?xml version='1.0' encoding='UTF-8'?>\n<X3D profile='Immersive' version='3.2'\n\
+  x3d = {xml: "<?xml version='1.0' encoding='UTF-8'?>\n", dom: []};
+
+  x3d.dom[0] = "<X3D profile='Immersive' version='3.2'\n\
     xmlns:xsd='http://www.w3.org/2001/XMLSchema-instance'\n\
     xsd:noNamespaceSchemaLocation='http://www.web3d.org/specifications/x3d-3.2.xsd'>\n";
   x3d.header = "\n\
     <head>\n\
       <meta content='"+name+".x3d' name='title'/>\n\
-      <meta content='X3D export from Bengler Terrafab' name='description'/>\n\
+      <meta content='X3D export from terrafab.bengler.no' name='description'/>\n\
       <meta content='"+(new Date()).toString()+"' name='created'/>\n\
       <meta content='"+(new Date()).toString()+"' name='modified'/>\n\
       <meta content='Bengler TerraFab' name='creator'/>\n\
@@ -30,14 +31,19 @@ function toX3D(geometry, name){
 
   x3d.scene = "\n\
     <Scene>\n"
-        x3d.scene += "         <Shape>\n\
-            <IndexedTriangleSet solid='false' index='";
+        x3d.scene += "         <Shape>\n";
+        x3d.scene += "          <Appearance>\n";
+        x3d.scene += "            <ImageTexture url=' \"uvmap.png\" \"http://terrafab.bengler.no/models/uv/"+name+".png\"'/>";
+        x3d.scene += "          </Appearance>";
+        x3d.scene += "            <IndexedTriangleSet solid='false' index='";
+        // Add index for triangles
         indicies = [];
         for(var i = 0; i<faces.length; i++){
           indicies.push([faces[i].a, faces[i].c, faces[i].b].join(' '));
         }
         x3d.scene += indicies.join(' ');
         x3d.scene += "'>\n";
+        // Add triangles
         x3d.scene += "              <Coordinate point='";
         points = [];
         for(var i = 0; i<vertices.length; i++){
@@ -46,10 +52,10 @@ function toX3D(geometry, name){
         x3d.scene += points.join(', ')
         x3d.scene += "'/>\n";
         x3d.scene += "            </IndexedTriangleSet>\n";
-        x3d.scene += "         </Shape>\n";
-  x3d.scene += "\
+        x3d.scene += "         </Shape>\n\
     </Scene>\n\n";
-  return x3d.doc+x3d.header+x3d.scene+'</X3D>\n';
+  x3d.dom[1] = '</X3D>\n';
+  return x3d.xml+x3d.dom[0]+x3d.header+x3d.scene+x3d.dom[1];
 }
 
 
