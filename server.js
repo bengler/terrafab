b7a27cc226fa6a75f4dac12d7049248105f8bfed
Bjørge Næss <bjoerge@bengler.no>
2013-09-24 10:34:14 +0200

    Move static middleware down in the hierarchy

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //provide browserified versions of all the files in a directory
 app.use('/js', browserify('./client', {
