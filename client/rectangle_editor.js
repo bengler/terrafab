ccc179ac55ae95a732e4ab636b5b2355a33fc2ce
Bjørge Næss <bjoerge@bengler.no>
2013-09-25 13:14:15 +0200

    Constrain to aspect ratio

--- client/rectangle_editor.js
+++ client/rectangle_editor.js
@@ -8,6 +8,7 @@ L.RectangleEditor = L.Polygon.extend ({
     weight: 1,
     fillColor: '#ffb400',
     fillOpacity: 0.2,
+    aspectRatio: 1
   },
 
   initialize: function(bounds, options) {
@@ -29,19 +30,17 @@ L.RectangleEditor = L.Polygon.extend ({
     var bounds = this.getBounds()
     this.setNorthWestAndSouthEast(bounds.getNorthWest(), bounds.getSouthEast())
     map.addLayer(markerGroup);
-    this._layoutResizeMarkers(this._latLngs);
-    this._layoutMoveMarker(this._latLngs);
   },
 
   createResizeMarkers: function () {
     var markers = {};
-    markers.sw = this.createMarker('sw', this._latLngs[SW]);
-    markers.ne = this.createMarker('ne', this._latLngs[NE]);
-    markers.se = this.createMarker('se', this._latLngs[SE]);
-    markers.nw = this.createMarker('nw', this._latLngs[NW]);
+    markers.sw = this.createResizeMarker('sw', this._latLngs[SW]);
+    markers.ne = this.createResizeMarker('ne', this._latLngs[NE]);
+    markers.se = this.createResizeMarker('se', this._latLngs[SE]);
+    markers.nw = this.createResizeMarker('nw', this._latLngs[NW]);
     return markers;
   },
-  createMarker: function (cornerClass, latLng) {
+  createResizeMarker: function (cornerClass, latLng) {
     var markerIcon = new L.DivIcon({
       iconSize: new L.Point(12, 12),
       className: 'leaflet-div-icon leaflet-editing-icon corner-'+cornerClass
@@ -50,7 +49,7 @@ L.RectangleEditor = L.Polygon.extend ({
       draggable: true,
       icon: markerIcon
     });
-    marker.on('drag', this._onMarkerDrag, this);
+    marker.on('drag', this._onResizeMarkerDrag, this);
     return marker;
   },
   onRemove: function (map) {
@@ -58,8 +57,7 @@ L.RectangleEditor = L.Polygon.extend ({
     L.Path.prototype.onRemove.call(this, map);
   },
   createMoveMarker: function(latlng) {
-    var icon = new L.Icon({
-      iconUrl: '/images/glyphicons_186_move.png',
+    var icon = new L.DivIcon({
       iconSize: [100,100],
       className: 'leaflet-div-icon leaflet-editing-icon moveable',
       cursor: 'move'
@@ -69,19 +67,19 @@ L.RectangleEditor = L.Polygon.extend ({
       icon: icon
     });
   },
-  _onMarkerDrag: function (e) {
+  _onResizeMarkerDrag: function (e) {
     var marker = e.target;
     if (marker == this._resizeMarkers.nw) {
-      this.setNorthWest(marker.getLatLng())
+      this.setNorthWest(this.constrainTo(marker.getLatLng(), this._latLngs[SE], this.options.aspectRatio))
     }
     else if (marker == this._resizeMarkers.se) {
-      this.setSouthEast(marker.getLatLng())
+      this.setSouthEast(this.constrainTo(marker.getLatLng(), this._latLngs[NW], this.options.aspectRatio))
     }
     else if (marker == this._resizeMarkers.sw) {
-      this.setSouthWest(marker.getLatLng())
+      this.setSouthWest(this.constrainTo(marker.getLatLng(), this._latLngs[NE], this.options.aspectRatio))
     }
     else if (marker == this._resizeMarkers.ne) {
-      this.setNorthEast(marker.getLatLng())
+      this.setNorthEast(this.constrainTo(marker.getLatLng(), this._latLngs[SW], this.options.aspectRatio))
     }
   },
   translateLatLngs: function(bounds) {
@@ -110,6 +108,7 @@ L.RectangleEditor = L.Polygon.extend ({
   setLatLngs: function(latLngs) {
     this._layoutResizeMarkers(latLngs);
     this._layoutMoveMarker(latLngs);
+    this._latLngs = latLngs;
     L.Polygon.prototype.setLatLngs.call(this, latLngs);
   },
   _layoutResizeMarkers: function(latLngs) {
@@ -119,8 +118,28 @@ L.RectangleEditor = L.Polygon.extend ({
     this._resizeMarkers.nw.setLatLng(latLngs[NW]);
     this.redraw()
   },
-  _layoutMoveMarker: function(latLngs) {
-    this._moveMarker.setLatLng(new L.LatLngBounds(latLngs).getCenter());
+  _layoutMoveMarker: function() {
+    var nwPoint = this._map.project(this._latLngs[NW]);
+    var sePoint = this._map.project(this._latLngs[SE]);
+
+    var center = this.getCenter()
+    var h = Math.abs(nwPoint.y - sePoint.y);
+    var w = Math.abs(nwPoint.x - sePoint.x);
+    var centerPoint = this._map.project(center);
+
+    nwPoint.y = centerPoint.y+(h/2);
+    nwPoint.x = centerPoint.x+(w/2);
+
+    sePoint.y = centerPoint.y-(h/2);
+    sePoint.x = centerPoint.x-(w/2);
+
+    this._moveMarker.setLatLng(center);
+    if (this._moveMarker._icon) {
+      this._moveMarker._icon.style.marginTop=(h/-2)+'px';
+      this._moveMarker._icon.style.marginLeft=(w/-2)+'px';
+      this._moveMarker._icon.style.height=(h)+'px';
+      this._moveMarker._icon.style.width=(w)+'px';      
+    } 
   },
   setNorthWestAndSouthEast: function(nwLatLng, seLatLng) {
     var nwPoint = this._map.project(nwLatLng);
@@ -134,7 +153,18 @@ L.RectangleEditor = L.Polygon.extend ({
 
     this.setLatLngs([nwLatLng, neLatLng, seLatLng, swLatLng])
   },
-
+  constrainTo: function(latLng, oppositeLatLng, aspectRatio) {
+    var point = this._map.project(latLng);
+    var oppositePoint = this._map.project(oppositeLatLng);
+    var diffX = Math.abs(oppositePoint.x - point.x) * aspectRatio;
+    var constrainedPoint;
+    if (oppositePoint.y < point.y) {
+      constrainedPoint = new L.Point(point.x, oppositePoint.y + diffX);
+    } else {
+      constrainedPoint =new L.Point(point.x, oppositePoint.y - diffX);
+    }
+    return this._map.unproject(constrainedPoint);
+  },
   setNorthWest: function(latLng) {
     this.setNorthWestAndSouthEast(latLng, this._latLngs[SE])
   },
